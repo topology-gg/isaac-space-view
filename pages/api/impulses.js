0b740e21e0cc7a7bf6ae1c6e218299e5b35980e5
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-03 06:24:15 +0800

    update db name

--- pages/api/impulses.js
+++ pages/api/impulses.js
@@ -7,7 +7,7 @@ const client = new MongoClient(MONGO_CONNECTION_STRING)
 export default async function handler(req, res) {
     await client.connect()
 
-    const db = client.db('isaac_alpha')
+    const db = client.db('isaac')
     const impulses = await db
         .collection('u0' + '_impulses')
         .find({'_chain.valid_to' : null})
--- pages/api/macro_state.js
+++ pages/api/macro_state.js
@@ -7,7 +7,7 @@ const client = new MongoClient(MONGO_CONNECTION_STRING)
 export default async function handler(req, res) {
     await client.connect()
 
-    const db = client.db('isaac_alpha')
+    const db = client.db('isaac')
     const macro_states = await db
         .collection('u0' + '_macro_states')
         .find({'_chain.valid_to' : null})
