655d4f1a8d7e38e15216ce274eb8d673906361e2
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-19 19:03:22 +0100

    update db name

--- pages/api/impulses.js
+++ pages/api/impulses.js
@@ -3,7 +3,8 @@ import clientPromise from "../../lib/mongodb"
 export default async function handler(req, res) {
     const client = await clientPromise
 
-    const db = client.db('isaac_10ce37b')
+    const db = client.db('isaac_dfbc16')
+    // const db = client.db('isaac_10ce37b')
     const impulses = await db
         .collection('u0' + '_impulses')
         .find({'_chain.valid_to' : null})
--- pages/api/macro_state.js
+++ pages/api/macro_state.js
@@ -3,7 +3,8 @@ import clientPromise from "../../lib/mongodb"
 export default async function handler(req, res) {
     const client = await clientPromise
 
-    const db = client.db('isaac_10ce37b')
+    const db = client.db('isaac_dfbc16')
+    // const db = client.db('isaac_10ce37b')
     const macro_states = await db
         .collection('u0' + '_macro_states')
         .find({'_chain.valid_to' : null})
